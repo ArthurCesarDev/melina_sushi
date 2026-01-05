cbafd89636037d5087fb6140cab36f12776fb3ce
ArthurCesarDev <arthurcesarx@outlook.com>
2026-01-05 19:00:05 -0300

    fix: ajustando toats e formatação de imagem antes do envio.

--- src/app/cardapio/components/MenuSection.tsx
+++ src/app/cardapio/components/MenuSection.tsx
@@ -1,23 +1,22 @@
 import CategorySection from "../components/CategorySection";
 import { products } from "@/lib/products";
 
-export default function MenuSection({ onAdd }: { onAdd: any }) {
+export default function MenuSection() {
   return (
     <>
       <CategorySection
         title="Pratos Empanados 🔥"
         products={products.filter((p) => p.category === "Pratos Quentes")}
-        onAdd={onAdd}
       />
+
       <CategorySection
         title="Pratos Crus"
         products={products.filter((p) => p.category === "Pratos Crus")}
-        onAdd={onAdd}
       />
+
       <CategorySection
         title="Monte seu Combo 🍱"
         products={products.filter((p) => p.category === "Combos")}
-        onAdd={onAdd}
       />
     </>
   );
